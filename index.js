e7de2865214cf18c85faab4d25c67f5347432ae3
RimoMimaru <vietthuannguyen760@gmail.com>
2025-12-05 12:39:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const mineflayer = require('mineflayer');
 const express = require('express');
 const app = express();
 
-// --- Web server để Railway giữ project awake ---
+// Web server giữ bot awake
 app.get('/', (req, res) => res.send('Bot Minecraft đang chạy 24/7!'));
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Web server chạy port ${port}`));
 
-// --- Bot Minecraft ---
+// Bot Minecraft offline mode
 function createBot() {
   const bot = mineflayer.createBot({
-    host: 'SMP_Sever_NH1.aternos.me', // thay bằng server Aternos của bạn
-    port: 42903,                     // port server Minecraft
-    username: 'Bot',        // tài khoản Microsoft/Mojang
+    host: 'SMP_Sever_NH1.aternos.me', // server offline
+    port: 42903,
+    username: 'Bot',
     auth: 'offline'
   });
 
   bot.on('spawn', () => console.log('Bot đã vào server!'));
-  bot.on('end', () => setTimeout(createBot, 5000)); // auto reconnect
+  bot.on('end', () => setTimeout(createBot, 5000));
 }
 
 createBot();
